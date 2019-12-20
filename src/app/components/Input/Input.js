f4b1a28b19fbbffe09ca8a3e0986f0313f15fc39
Manindu Wijewickrama <maninduw@Manindus-MacBook-Air.local>
2019-12-20 18:54:14 +0530

    added props to input

--- src/app/components/Input/Input.js
+++ src/app/components/Input/Input.js
@@ -2,21 +2,35 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './Input.scss';
 
-const Input = ({ label, placeholder }) => (
+const Input = ({ id, label, placeholder, onChange, onBlur, value, type }) => (
   <div className="input-container">
     <p className="input-label">{label}</p>
-    <input className="input" placeholder={placeholder} />
+    <input
+      id={id}
+      className="input"
+      placeholder={placeholder}
+      onChange={onChange}
+      onBlur={onBlur}
+      value={value}
+      type={type}
+    />
   </div>
 );
 
 Input.propTypes = {
+  id: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+  onBlur: PropTypes.func.isRequired,
+  value: PropTypes.oneOfType([PropTypes.number, PropTypes.string]).isRequired,
+  type: PropTypes.string,
   label: PropTypes.string,
   placeholder: PropTypes.string
 };
 
 Input.defaultProps = {
   label: 'Input',
-  placeholder: 'John Doe'
+  placeholder: 'John Doe',
+  type: 'text'
 };
 
 export default Input;
--- src/app/pages/Payment/Payment.js
+++ src/app/pages/Payment/Payment.js
@@ -1,43 +1,75 @@
 import React from 'react';
 import { Col, Row, Container } from 'react-grid-system';
+import { Formik } from 'formik';
+import * as Yup from 'yup';
 import { Input, CardInput, Button } from 'app/components';
 import './Payment.scss';
 
-const Payment = () => (
-  <div className="payment-page">
-    <div className="form">
-      <Container>
-        <Row>
-          <Col sm={12} md={12} lg={12}>
-            <Input label="Name on Card" />
-          </Col>
-        </Row>
-        <Row>
-          <Col sm={12} md={12} lg={12}>
-            <CardInput label="Card Number" />
-          </Col>
-        </Row>
-        <Row>
-          <Col sm={4}>
-            <Input label="Expiry Date" placeholder="10" />
-          </Col>
-          <Col sm={4}>
-            <Input placeholder="2019" />
-          </Col>
-          <Col sm={4}>
-            <Input label="CVV" placeholder="444" />
-          </Col>
-        </Row>
-        <Row>
-          <Col sm={12} md={12} lg={12}>
-            <div className="button-section">
-              <Button />
-            </div>
-          </Col>
-        </Row>
-      </Container>
-    </div>
-  </div>
-);
+const Payment = () => {
+  const onSubmit = values => {
+    console.log(values);
+  };
+
+  return (
+    <Formik
+      initialValues={{
+        name: ''
+      }}
+      validationSchema={Yup.object().shape({
+        name: Yup.string().required()
+      })}
+      onSubmit={onSubmit}
+      render={({
+        handleChange,
+        handleBlur,
+        values
+        // errors,
+        // handleSubmit,
+        // touched
+      }) => (
+        <div className="payment-page">
+          <div className="form">
+            <Container>
+              <Row>
+                <Col sm={12} md={12} lg={12}>
+                  <Input
+                    id="nameOnCard"
+                    label="Name on Card"
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    value={values.name}
+                  />
+                </Col>
+              </Row>
+              <Row>
+                <Col sm={12} md={12} lg={12}>
+                  <CardInput label="Card Number" />
+                </Col>
+              </Row>
+              <Row>
+                <Col sm={4}>
+                  <Input label="Expiry Date" placeholder="10" />
+                </Col>
+                <Col sm={4}>
+                  <Input placeholder="2019" />
+                </Col>
+                <Col sm={4}>
+                  <Input label="CVV" placeholder="444" />
+                </Col>
+              </Row>
+              <Row>
+                <Col sm={12} md={12} lg={12}>
+                  <div className="button-section">
+                    <Button />
+                  </div>
+                </Col>
+              </Row>
+            </Container>
+          </div>
+        </div>
+      )}
+    />
+  );
+};
 
 export default Payment;
